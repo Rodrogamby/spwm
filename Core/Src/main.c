18efc15db8c81d4756e2b5b5f259909ee9b798b8
Rodrigo Gamboa <rodrigo.gamboa@Outlook.com>
2025-04-27 21:28:23 -0600

    implement bipolar spwm

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,4 +1,5 @@
 /* USER CODE BEGIN Header */
+#include <stdbool.h>
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
@@ -41,11 +42,17 @@ static void MX_TIM3_Init(void);
 static void MX_TIM6_Init(void);
 /* USER CODE BEGIN PFP */
 static void toggleUserLed(void);
+static void flipGround(void);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-
+TIM_OC_InitTypeDef sConfigOC = {0};
+bool state_positive = true;
+uint16_t lookup_index = 0;
+float amplitude = 1; // adjust accordingly
+const uint16_t lookup_size = 360; // must match array size below, which is auto-generated
+const uint16_t lookup_table[360] = { 0, 8, 17, 26, 34, 43, 52, 61, 69, 78, 87, 95, 104, 113, 121, 130, 139, 147, 156, 165, 173, 182, 190, 199, 207, 216, 224, 233, 241, 250, 258, 267, 275, 284, 292, 300, 309, 317, 325, 333, 342, 350, 358, 366, 374, 382, 390, 398, 406, 414, 422, 430, 438, 446, 453, 461, 469, 477, 484, 492, 499, 507, 515, 522, 529, 537, 544, 551, 559, 566, 573, 580, 587, 594, 601, 608, 615, 622, 629, 636, 642, 649, 656, 662, 669, 675, 681, 688, 694, 700, 707, 713, 719, 725, 731, 737, 743, 748, 754, 760, 766, 771, 777, 782, 788, 793, 798, 803, 809, 814, 819, 824, 829, 833, 838, 843, 848, 852, 857, 861, 866, 870, 874, 878, 882, 887, 891, 894, 898, 902, 906, 909, 913, 917, 920, 923, 927, 930, 933, 936, 939, 942, 945, 948, 951, 953, 956, 958, 961, 963, 965, 968, 970, 972, 974, 976, 978, 979, 981, 983, 984, 986, 987, 989, 990, 991, 992, 993, 994, 995, 996, 996, 997, 998, 998, 999, 999, 999, 999, 999, 1000, 999, 999, 999, 999, 999, 998, 998, 997, 996, 996, 995, 994, 993, 992, 991, 990, 989, 987, 986, 984, 983, 981, 979, 978, 976, 974, 972, 970, 968, 965, 963, 961, 958, 956, 953, 951, 948, 945, 942, 939, 936, 933, 930, 927, 923, 920, 917, 913, 909, 906, 902, 898, 894, 891, 887, 882, 878, 874, 870, 866, 861, 857, 852, 848, 843, 838, 833, 829, 824, 819, 814, 809, 803, 798, 793, 788, 782, 777, 771, 766, 760, 754, 748, 743, 737, 731, 725, 719, 713, 707, 700, 694, 688, 681, 675, 669, 662, 656, 649, 642, 636, 629, 622, 615, 608, 601, 594, 587, 580, 573, 566, 559, 551, 544, 537, 529, 522, 515, 507, 500, 492, 484, 477, 469, 461, 453, 446, 438, 430, 422, 414, 406, 398, 390, 382, 374, 366, 358, 350, 342, 333, 325, 317, 309, 300, 292, 284, 275, 267, 258, 250, 241, 233, 224, 216, 207, 199, 190, 182, 173, 165, 156, 147, 139, 130, 121, 113, 104, 95, 87, 78, 69, 61, 52, 43, 34, 26, 17, 8 };
 /* USER CODE END 0 */
 
 /**
@@ -81,7 +88,7 @@ int main(void)
   MX_TIM6_Init();
   /* USER CODE BEGIN 2 */
 
-  //HAL_TIM_Base_Start(&htim6);
+  HAL_TIM_Base_Start_IT(&htim6);
 
   /* USER CODE END 2 */
 
@@ -104,8 +111,11 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
-  HAL_TIM_Base_Start_IT(&htim6);
+  /*
+   * Timer 6 interval should be steps / 2*output_frequency
+   */
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_1);
+  __HAL_TIM_ENABLE_IT(&htim6, TIM_IT_UPDATE);
   while (1)
   {
     /* USER CODE END WHILE */
@@ -177,12 +187,12 @@ static void MX_TIM3_Init(void)
 
   TIM_ClockConfigTypeDef sClockSourceConfig = {0};
   TIM_MasterConfigTypeDef sMasterConfig = {0};
-  TIM_OC_InitTypeDef sConfigOC = {0};
+  //TIM_OC_InitTypeDef sConfigOC = {0};
 
   /* USER CODE BEGIN TIM3_Init 1 */
   /* USER CODE END TIM3_Init 1 */
   htim3.Instance = TIM3;
-  htim3.Init.Prescaler = 128;
+  htim3.Init.Prescaler = 0;
   htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim3.Init.Period = 1000;
   htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
@@ -207,7 +217,7 @@ static void MX_TIM3_Init(void)
     Error_Handler();
   }
   sConfigOC.OCMode = TIM_OCMODE_PWM1;
-  sConfigOC.Pulse = 500;
+  sConfigOC.Pulse = 0;
   sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
   sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
   if (HAL_TIM_PWM_ConfigChannel(&htim3, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
@@ -238,9 +248,9 @@ static void MX_TIM6_Init(void)
 
   /* USER CODE END TIM6_Init 1 */
   htim6.Instance = TIM6;
-  htim6.Init.Prescaler = 256;
+  htim6.Init.Prescaler = 64 - 1;
   htim6.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim6.Init.Period = 65535;
+  htim6.Init.Period = 25;
   htim6.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_Base_Init(&htim6) != HAL_OK)
   {
@@ -296,7 +306,7 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct); // Claim PA5
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct); // Claim PA5 (user LED)
   /* USER CODE END MX_GPIO_Init_2 */
 }
 
@@ -306,13 +316,44 @@ void toggleUserLed(void)
 	HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
 }
 
-void HAL_TIM_PeriodEllapsedCallback(TIM_HandleTypeDef *htim)
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 {
 	if(htim == &htim6)
 	{
 		// Toggle something maybe
 		toggleUserLed();
+		lookup_index++;
+		if(lookup_index >= lookup_size)
+		{
+			lookup_index = 0;
+			if(state_positive)
+			{
+				sConfigOC.Pulse = htim3.Init.Period;
+			}
+			else
+			{
+				sConfigOC.Pulse = 0;
+			}
+			HAL_TIM_PWM_ConfigChannel(&htim3, &sConfigOC, TIM_CHANNEL_1);
+			flipGround(); // Do this here with zero to account for dead time
+			return;
+		}
+		if(state_positive)
+		{
+			sConfigOC.Pulse = lookup_table[lookup_index];
+		}
+		else
+		{
+			sConfigOC.Pulse = htim3.Init.Period - lookup_table[lookup_index];
+		}
+		HAL_TIM_PWM_ConfigChannel(&htim3, &sConfigOC, TIM_CHANNEL_1);
 	}
+}
+
+static void flipGround(void)
+{
+	state_positive ^= 1;
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8, !state_positive);
 }
 /* USER CODE END 4 */
 
